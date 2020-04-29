7a3cb17e32e3b1c55f16507052cc449a1c8da90a
Soichi Hayashi <hayashis@login04.osgconnect.net>
2020-04-29 13:08:10 -0500

    trimming first 4 lines out of pegasus_analyzer

--- status.sh
+++ status.sh
@@ -5,8 +5,6 @@
 #return code 2 = failed
 #return code 3 = unknown status
 
-#pegasus-status -l work
-
 if [ -f finished ]; then
     echo "already finished"
     exit 1
@@ -23,23 +21,14 @@ jobstate=$(cat .status | tail -2  | head -1)
 if [ -z "$jobstate" ]; then
 	exit 2 #removed?
 fi
-#if [ $jobstate == "QUEUED" ]; then
-#	exit 0
-#fi
 if [ "$jobstate" == "Running" ]; then
 	cat .status | grep "Run "
-	pegasus-analyzer work
+	pegasus-analyzer work | tail -n +4
 	exit 0
 fi
-#if [ $jobstate == "DELETE PENDING" ]; then
-#	exit 2
-#fi
-#if [ $jobstate == "DELETED" ]; then
-#	exit 2
-#fi
 if [ "$jobstate" == "Failure" ]; then
 	#TODO - pegasus reports Failure even if I stop the workflow.. (no "Stopped"?)
-	pegasus-analyzer work
+	pegasus-analyzer work | tail -n +4
 	exit 2
 fi
 if [ "$jobstate" == "Success" ]; then
@@ -47,11 +36,6 @@ if [ "$jobstate" == "Success" ]; then
 	tar -xf subject_output.tar.bz2
 	exit 1
 fi
-
-#assume failed for all other state
-#'ERROR'
-#'FAILED',
-
 echo "can't determine the status! $jobstate"
 exit 3
 
